bc9e596e6aca5f811de6951099e67b235bddc051
mauriciomolinapicco <mauriscode@gmail.com>
2025-02-16 10:59:43 -0300

    finished crud

--- inventory/main.py
+++ inventory/main.py
@@ -33,8 +33,28 @@ class Product(HashModel):
 
 @app.get("/products")
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return { 
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
 
 @app.post("/products")
 def create(product: Product):
     return product.save()
+
+
+@app.get("/products/{pk}")
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete("/products/{pk}")
+def delete(pk: str):
+    return Product.delete(pk)
